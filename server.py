6cf91ad6efbf6e1c5f4d13e43210bc4aa787a52d
rohilrao <rohil93@gmail.com>
2025-04-28 16:49:32 +0200

    add uv mcp server demo

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+# server.py
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
-mcp = FastMCP(name = "Demo", host = '0.0.0.0', port = 6275)
+mcp = FastMCP("Demo")
+
 
 # Add an addition tool
 @mcp.tool()
@@ -9,21 +11,9 @@ def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
+
 # Add a dynamic greeting resource
 @mcp.resource("greeting://{name}")
 def get_greeting(name: str) -> str:
     """Get a personalized greeting"""
     return f"Hello, {name}!"
-
-# 👇 ADD THIS to actually start the server
-if __name__ == "__main__":
-    transport = "sse"
-    if transport == "stdio":
-        print("Starting server with stdio transport...")
-        mcp.run(transport=  "stdio")
-    elif transport == "sse":
-        print("Starting server with SSE transport...")
-        mcp.run(transport=  "sse")
-    else:
-        raise ValueError("Invalid transport type. Use 'stdio' or 'sse'.")
-    
